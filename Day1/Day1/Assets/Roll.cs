99026c471bffcd2ab05e757d3a5f277f7012f83c
GanesanNS <ganesan@arivuventures.com>
2017-05-07 17:17:23 +0530

    Initial Commit

---
+++ Day1/Day1/Assets/Roll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour {
+
+	public float movementSpeed = 1000;
+
+	private Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+
+		this.myRigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Vector3.left);
+
+		//if (Vector3.left <= 10) {
+			transform.Translate (Vector3.left * movementSpeed * Time.deltaTime);
+
+
+		
+		//}
+
+//		float rotation = Input.GetAxis ("Horizontal");
+//		rotation *= Time.deltaTime * movementSpeed;
+//
+//		this.myRigidbody.AddRelativeTorque(Vector3.back * rotation);
+
+	}
+}
